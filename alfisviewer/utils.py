b9b02b26b5d3dab09e317f2ace01c8d37c8224d1
CI <builds@builds.sr.ht>
2021-12-27 08:07:40 +0000

    Applied BLACK code style

--- alfisviewer/utils.py
+++ alfisviewer/utils.py
@@ -5,4 +5,5 @@ import os
 def serve_file(file, content_type):
     def serve_file_middle(request):
         return serve(request, os.path.basename(file), os.path.dirname(file))
+
     return serve_file_middle
