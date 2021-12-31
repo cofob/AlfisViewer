97af7d813be5c0295dc8b8f0bbb50a54c8f5e779
CI <builds@builds.sr.ht>
2021-12-31 18:27:49 +0000

    Applied BLACK code style

--- index/views.py
+++ index/views.py
@@ -112,15 +112,17 @@ def set_lang(request, lang_code):
     resp = {}
     lang_code = lang_code.lower()
     if len(lang_code) != 2:
-        return JsonResponse({'ok': False, 'error': 'Language code can be only 2 characters length.'})
+        return JsonResponse(
+            {"ok": False, "error": "Language code can be only 2 characters length."}
+        )
     if lang_code not in settings.LANGUAGE_CODES:
         lang = "en"
-        resp['warn'] = 'Language not found, using default.'
+        resp["warn"] = "Language not found, using default."
     else:
         lang = lang_code
     translation.activate(lang)
-    resp['ok'] = True
-    resp['lang'] = lang
+    resp["ok"] = True
+    resp["lang"] = lang
     response = JsonResponse(resp)
     response.set_cookie(settings.LANGUAGE_COOKIE_NAME, lang)
     return response
