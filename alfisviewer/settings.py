cc61fb6fdec2f0d9d2200c5460c3641190216e71
cofob <c@cofob.ru>
2021-12-31 19:25:56 +0700

    Revert "Renamed locales"
    This reverts commit 021dd1bc624e071baa2a7c5beb2ccea97861d43b.

--- alfisviewer/settings.py
+++ alfisviewer/settings.py
@@ -166,7 +166,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = "en"
+LANGUAGE_CODE = "en-us"
 
 LANGUAGES = (
     ("en", "English"),
