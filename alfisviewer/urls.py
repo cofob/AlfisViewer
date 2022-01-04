4279c1210e1a722aafa1e52953442a3f6618f889
cofob <c@cofob.ru>
2022-01-05 03:13:32 +0700

    Fixed blockchain path

--- alfisviewer/urls.py
+++ alfisviewer/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from .utils import serve_file
-from django.conf.urls.i18n import i18n_patterns
+from alfis_connector import DB_FILE
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -15,7 +15,7 @@ urlpatterns = [
     path("settings/", include("settings_app.urls")),
     path(
         "blockchain.db",
-        serve_file("blockchain.db", "application/x-sqlite3"),
+        serve_file(DB_FILE, "application/x-sqlite3"),
         name="blockchain",
     ),
     path("favicon.ico", serve_file("favicon.ico", "image/x-icon")),
